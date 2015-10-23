b2dfee97d838aae5588dad3e72420a7b22e894ff
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2015-10-23 22:23:54 +0100

    Arreglada indentacion

--- test/tc_fraccionarios.rb
+++ test/tc_fraccionarios.rb
@@ -12,11 +12,11 @@ class TestPoint < Test::Unit::TestCase
   end
   def test_mcd
     assert_equal(5, @numero_fraccionario_1.mcd(10,5))
-	assert_equal(8, @numero_fraccionario_1.mcd(40,8))
+    assert_equal(8, @numero_fraccionario_1.mcd(40,8))
   end
   def test_mcm
     assert_equal(5, @numero_fraccionario_1.mcm(5,1))
-	assert_equal(60, @numero_fraccionario_2.mcm(15,6))
+    assert_equal(60, @numero_fraccionario_2.mcm(15,6))
   end
   def test_suma
     assert_equal("40/10", (@numero_fraccionario_1 + @numero_fraccionario_2).to_s)
