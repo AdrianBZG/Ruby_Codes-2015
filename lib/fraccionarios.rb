d0cacf1d3302e6064f7c8e5361c46873b505abb8
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2015-10-23 23:02:49 +0100

    Indentado correctamente

--- lib/fraccionarios.rb
+++ lib/fraccionarios.rb
@@ -1,62 +1,62 @@
 class Numero_Fraccionario
   attr_accessor :numerador, :denominador
   def initialize(numerador, denominador)
-    @numerador, @denominador = numerador, denominador
+  	@numerador, @denominador = numerador, denominador
   end
   def to_s
-    "#{@x}/#{@y}"
+  	"#{@x}/#{@y}"
   end
   def mcd(valor1, valor2)
-	if valor1 == 0
+  	if valor1 == 0
 		valor2
 	else
 		mcd(valor2%valor1, valor1)
 	end
   end
   def mcm(valor1, valor2)
-    (valor1/mcd(valor1,valor2))*valor2
+  	(valor1/mcd(valor1,valor2))*valor2
   end  
   def + (other)
 	aux = mcm(@denominador, other.denominador)
-    Numero_Fraccionario.new(((aux/@denominador)*@numerador)+((aux/other.denominador)*other.numerador), aux)
+	Numero_Fraccionario.new(((aux/@denominador)*@numerador)+((aux/other.denominador)*other.numerador), aux)
   end
   def - (other)
-	aux = mcm(@denominador, other.denominador)
-    Numero_Fraccionario.new(((aux/other.denominador)*other.numerador)-((aux/@denominador)*@numerador), aux)
+  	aux = mcm(@denominador, other.denominador)
+	Numero_Fraccionario.new(((aux/other.denominador)*other.numerador)-((aux/@denominador)*@numerador), aux)
   end
   def * (other)
-    Numero_Fraccionario.new((@numerador * other.numerador),(@denominador * other.denominador))
+  	Numero_Fraccionario.new((@numerador * other.numerador),(@denominador * other.denominador))
   end
   def / (other)
-    Numero_Fraccionario.new((@numerador * other.denominador),(@denominador * other.numerador))
+  	Numero_Fraccionario.new((@numerador * other.denominador),(@denominador * other.numerador))
   end
   def simplificar
-    aux = mcd(@numerador,@denominador)
-	Numero_Fraccionario.new(@numerador/aux, @denominador/aux)
+  	aux = mcd(@numerador,@denominador)
+  	Numero_Fraccionario.new(@numerador/aux, @denominador/aux)
   end
   def igual_que (other)
-    if @denominador == other.denominador and @numerador == other.numerador
-		true
-	else
-		false
-	end
+  	if @denominador == other.denominador and @numerador == other.numerador
+  		true
+  	else
+  		false
+  	end
   end
   def distinto_que (other)
-    if @denominador == other.denominador and @numerador == other.numerador
-		false
-	else
-		true
-	end
+  	if @denominador == other.denominador and @numerador == other.numerador
+  		false
+  	else
+  		true
+  	end
   end
   def menor_que (other)
-    if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
-		true
+  	if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
+  		true
 	else
 		false
 	end
   end
   def mayor_que (other)
-    if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
+  	if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
 		false
 	else
 		true
