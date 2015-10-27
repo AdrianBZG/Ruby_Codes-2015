ae7f2ac407ff7d7b305fef0d58dbdae78e8c9270
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2015-10-27 08:56:38 +0000

    Arreglos en la sintaxis

--- lib/fraccionarios.rb
+++ lib/fraccionarios.rb
@@ -17,46 +17,46 @@ class Numero_Fraccionario
   	(valor1/mcd(valor1,valor2))*valor2
   end  
   def + (other)
-	aux = mcm(@denominador, other. denominador)
-	Numero_Fraccionario.new(((aux/@denominador)*@numerador)+((aux/other. denominador)*other. numerador), aux)
+	aux = mcm(@denominador, other.denominador)
+	Numero_Fraccionario.new(((aux/@denominador)*@numerador)+((aux/other.denominador)*other.numerador), aux)
   end
   def - (other)
-  	aux = mcm(@denominador, other. denominador)
-	Numero_Fraccionario.new(((aux/other. denominador)*other. numerador)-((aux/@denominador)*@numerador), aux)
+  	aux = mcm(@denominador, other.denominador)
+	Numero_Fraccionario.new(((aux/other.denominador)*other.numerador)-((aux/@denominador)*@numerador), aux)
   end
   def * (other)
-  	Numero_Fraccionario.new((@numerador * other. numerador),(@denominador * other. denominador))
+  	Numero_Fraccionario.new((@numerador * other.numerador),(@denominador * other.denominador))
   end
   def / (other)
-  	Numero_Fraccionario.new((@numerador * other. denominador),(@denominador * other. numerador))
+  	Numero_Fraccionario.new((@numerador * other.denominador),(@denominador * other.numerador))
   end
   def simplificar
   	aux = mcd(@numerador,@denominador)
   	Numero_Fraccionario.new(@numerador/aux, @denominador/aux)
   end
   def igual_que (other)
-  	if @denominador == other. denominador and @numerador == other. numerador
+  	if @denominador == other.denominador and @numerador == other.numerador
   		true
   	else
   		false
   	end
   end
   def distinto_que (other)
-  	if @denominador == other. denominador and @numerador == other. numerador
+  	if @denominador == other.denominador and @numerador == other.numerador
   		false
   	else
   		true
   	end
   end
   def menor_que (other)
-  	if @denominador > other. denominador || (@denominador == other. denominador and @numerador < other. numerador)
+  	if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
   		true
 	else
 		false
 	end
   end
   def mayor_que (other)
-  	if @denominador > other. denominador || (@denominador == other. denominador and @numerador < other. numerador)
+  	if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
 		false
 	else
 		true
