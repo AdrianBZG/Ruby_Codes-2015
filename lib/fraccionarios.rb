f4893933866c35e55e83e14c156552d65007441d
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2015-10-27 09:02:26 +0000

    Comentarios añadidos al código

--- lib/fraccionarios.rb
+++ lib/fraccionarios.rb
@@ -1,12 +1,20 @@
+# Clase "Numero Fraccionario"
+# Adrián Rodríguez Bazaga
+
 class Numero_Fraccionario
+  # Se tiene acceso de lectura y escritura a 'numerador' y 'denominador'
   attr_accessor :numerador, :denominador
   def initialize(numerador, denominador)
+  	# Constructor: Los fraccionarios no se simplifican en su inicialización, se debe hacer intencionadamente
+  	# con el método 'simplificar'
   	@numerador, @denominador = numerador, denominador
   end
   def to_s
+  	#Mostrar numerador y denominador
   	"#{@x}/#{@y}"
   end
   def mcd(valor1, valor2)
+  	#Maximo comun divisor
   	if valor1 == 0
 		valor2
 	else
@@ -14,27 +22,34 @@ class Numero_Fraccionario
 	end
   end
   def mcm(valor1, valor2)
+  	#Minimo comun multiplo
   	(valor1/mcd(valor1,valor2))*valor2
   end  
   def + (other)
+  	#Suma de dos objetos tipo numero fraccionario
 	aux = mcm(@denominador, other.denominador)
 	Numero_Fraccionario.new(((aux/@denominador)*@numerador)+((aux/other.denominador)*other.numerador), aux)
   end
   def - (other)
+  	#Resta de dos objetos tipo numero fraccionario
   	aux = mcm(@denominador, other.denominador)
 	Numero_Fraccionario.new(((aux/other.denominador)*other.numerador)-((aux/@denominador)*@numerador), aux)
   end
   def * (other)
+  	#Multiplicación de dos objetos tipo numero fraccionario
   	Numero_Fraccionario.new((@numerador * other.numerador),(@denominador * other.denominador))
   end
   def / (other)
+  	#División de dos objetos tipo numero fraccionario
   	Numero_Fraccionario.new((@numerador * other.denominador),(@denominador * other.numerador))
   end
   def simplificar
+  	#Simplificación de un numero fraccionario (objeto)
   	aux = mcd(@numerador,@denominador)
   	Numero_Fraccionario.new(@numerador/aux, @denominador/aux)
   end
   def igual_que (other)
+  	#Comparador de igualdad entre dos objetos de tipo numero fraccionario
   	if @denominador == other.denominador and @numerador == other.numerador
   		true
   	else
@@ -42,6 +57,7 @@ class Numero_Fraccionario
   	end
   end
   def distinto_que (other)
+  	#Comparador de distinto entre dos objetos de tipo numero fraccionario
   	if @denominador == other.denominador and @numerador == other.numerador
   		false
   	else
@@ -49,6 +65,7 @@ class Numero_Fraccionario
   	end
   end
   def menor_que (other)
+  	#Comparador de menor que entre dos objetos de tipo numero fraccionario
   	if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
   		true
 	else
@@ -56,6 +73,7 @@ class Numero_Fraccionario
 	end
   end
   def mayor_que (other)
+  	#Comparador de mayor que entre dos objetos de tipo numero fraccionario
   	if @denominador > other.denominador || (@denominador == other.denominador and @numerador < other.numerador)
 		false
 	else
